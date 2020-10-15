1b16eb5fc13326877597bb198eba38799c6b9853
Mohammad Ahmadi <52349443+Momo10-barca@users.noreply.github.com>
2020-10-15 22:22:34 +0200

    uploading files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,98 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Particles from "react-tsparticles";
+
+
+const costumParticles = {
+        
+              fpsLimit: 60,
+              interactivity:{
+                detectsOn: "canvas",
+                events:{
+                  onClick:{
+                    enable: true,
+                    mode: "push"
+                  },
+                  onHover:{
+                    enable: true,
+                    mode: ["grab", "repulse"]
+                  },
+                  resize: true
+                },
+                modes:{
+                  repulse:{
+                    distance: 100,
+                    duration: 2,
+                    opacity: 0.8,
+                    size: 10
+                  },
+                  push:{
+                    quantity: 4
+                  },
+                  grap:{
+                    distance: 100
+                  }
+                  
+                }
+              },
+              particles:{
+                color:{
+                  value: "#ffffffff"
+                },
+                links:{
+                  color: "#ffffffff",
+                  distance: 150,
+                  enable: true,
+                  opacity: 0.5,
+                  width: 1
+                },
+                collisions:{
+                  enable: true
+                },
+                move:{
+                  direction: "none",
+                  enable: true,
+                  outMode: "bounce",
+                  random: false,
+                  speed: 2
+                },
+                number:{
+                  density:{
+                  },
+                  value: 300
+                },
+                opacity:{
+                  value: 0.5
+                },
+                shape:{
+                  type: "circle"
+                },
+                size:{
+                  random: true,
+                  value:3
+                }
+              },
+              detectRetina: true
+          
+}
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Particles
+         id="tsparticles"
+         options={
+            costumParticles
+         }
+      />
+      
+      <div className="btn-div">
+        <button onClick={() => console.log("cliekd")}>hier</button>
+      </div>
+
+
     </div>
   );
 }
